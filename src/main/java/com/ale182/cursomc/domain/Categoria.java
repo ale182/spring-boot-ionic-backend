fb6062e107a8d37fdaba88d25fed4471d2c2255f
ale182 <alestalves@gmail.com>
2019-06-14 01:04:54 -0300

    Produto e associacao M to M

--- src/main/java/com/ale182/cursomc/domain/Categoria.java
+++ src/main/java/com/ale182/cursomc/domain/Categoria.java
@@ -1,11 +1,14 @@
 package com.ale182.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable {
@@ -16,6 +19,10 @@ public class Categoria implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer Id;
 	private String Nome;
+	
+	@ManyToMany(mappedBy="categorias")
+	private List<Produto> produtos = new ArrayList<>();
+	
 	
 	public Categoria () {
 		
@@ -43,6 +50,14 @@ public class Categoria implements Serializable {
 		Nome = nome;
 	}
 
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -67,6 +82,8 @@ public class Categoria implements Serializable {
 			return false;
 		return true;
 	}
+
+
 	
 	
 }
