78e2c19d7e8ff5b1cc83df758778a0f0453e441c
ale182 <alestalves@gmail.com>
2019-09-04 22:24:02 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/ale182/cursomc/services/PedidoService.java
@@ -0,0 +1,29 @@
+package com.ale182.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ale182.cursomc.domain.Pedido;
+import com.ale182.cursomc.repositories.PedidoRepository;
+import com.ale182.cursomc.services.exceptions.ObjectNotFoundException;
+// import java.util.Optional;
+		
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo ;
+	
+	public Pedido buscar(Integer id) {
+		Pedido obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id:" + id 
+					+ " , Tipo: " + Pedido.class.getName()
+					) ;
+		}
+			
+		return obj;
+	
+	}
+	
+}
