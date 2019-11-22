03120217b468164c057a935fda7d007f9ae5bc95
ale182 <alestalves@gmail.com>
2019-11-22 00:23:17 -0300

    Inserindo Cliente com POST

--- src/main/java/com/ale182/cursomc/services/ClienteService.java
+++ src/main/java/com/ale182/cursomc/services/ClienteService.java
@@ -2,6 +2,8 @@ package com.ale182.cursomc.services;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
@@ -9,10 +11,15 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.ale182.cursomc.domain.Cidade;
 import com.ale182.cursomc.domain.Cliente;
-import com.ale182.cursomc.domain.Cliente;
+import com.ale182.cursomc.domain.Endereco;
+import com.ale182.cursomc.domain.enums.TipoCliente;
 import com.ale182.cursomc.dto.ClienteDTO;
+import com.ale182.cursomc.dto.ClienteNewDTO;
+import com.ale182.cursomc.repositories.CidadeRepository;
 import com.ale182.cursomc.repositories.ClienteRepository;
+import com.ale182.cursomc.repositories.EnderecoRepository;
 import com.ale182.cursomc.services.exceptions.DataIntegrityException;
 import com.ale182.cursomc.services.exceptions.ObjectNotFoundException;
 // import java.util.Optional;
@@ -22,6 +29,13 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository repo ;
+	
+	// UPDATE do curso, para nao usar o cidadeRepository, mas direto a cidade
+	//@Autowired
+	//private CidadeRepository cidadeRepository ;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository ;	
 	
 	public Cliente find(Integer id) {
 		Cliente obj = repo.findOne(id);
@@ -35,6 +49,16 @@ public class ClienteService {
 	
 	}
 
+	// anotacao para salvar na mesma transacao do DB
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		// salva os endereços no banco, antes de retornar o insert do método
+		enderecoRepository.save(obj.getEnderecos());
+		return  obj ;
+	}
+	
 	public Cliente update(Cliente obj) {
 	 	// Alterado para alterar o CPF, instanciando um Cliente a partir do Banco de DAdos
 		// ANTERIOR = find(obj.getId());
@@ -68,6 +92,34 @@ public class ClienteService {
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId() , objDto.getNome() , objDto.getEmail(),null , null);
 	}
+	
+	// sobrecarga do método , usando a classe ClienteNewDTO
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		
+		// ID é nulo, pq está na inserção, auto_increment
+		Cliente cli = new Cliente(null , objDto.getNome() , objDto.getEmail(),objDto.getCpfOuCnpj() , TipoCliente.toEnum(objDto.getTipo()));
+		// UPDATE do curso, para nao usar o cidadeRepository, mas direto a cidade
+		//Cidade cid = cidadeRepository.findOne(objDto.getCidadeId());
+		Cidade cid = new Cidade(objDto.getCidadeId(),null,null);
+		Endereco end = new Endereco(null,objDto.getLogradouro() , objDto.getNumero() , objDto.getComplemento() , objDto.getBairro(),objDto.getCep(),cli,cid);
+		
+		// incluir endereço na lista de endereços do Cliente
+		cli.getEnderecos().add(end);
+		
+		// incluir telefones no Cliente
+		cli.getTelefones().add(objDto.getTelefone1());
+		
+		if (objDto.getTelefone2()!=null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+
+		if (objDto.getTelefone3()!=null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		
+		return cli ;
+	}
+	
 	
 	private void updateData(Cliente newObj , Cliente obj) {
 		// busca os dados do Objeto atual para atualizar no novo
